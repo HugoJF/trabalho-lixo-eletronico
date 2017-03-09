586cfc43bc2d772e4c31b972353d41a1b9f11e8a
Hugo Jeller <hugo_jeller@hotmail.com>
2017-03-08 22:04:36 -0400

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+/* #### INFORMATIONS #### */
+    Route::get('/informacoes/{name?}', 'InformationsController@listInformations')->name('informations');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* #### LOCATIONS #### */
+    // Submission form
+    Route::get('/locais/submeter', 'LocationsController@submitLocationForm')->name('submit-location-form');
+    // Location view
+    Route::get('/locais/id/{id}', 'LocationsController@view')->name('location-view');
+    // List locations from state
+    Route::get('/locais/{state}', 'LocationsController@listLocations')->name('locations');
+    // List current states
+    Route::get('/locais/', 'LocationsController@listStates')->name('locations-all');
+    // Submission POST
+    Route::post('/locais/submeter', 'LocationsController@submitLocation')->name('submit-location');
+
+/* #### AUTHENTICATION #### */
+    Auth::routes();
+    Route::get('/logout', 'UsersController@logout');
+
+
+/* #### OTHERS #### */
+    Route::get('/dashboard', 'HomeController@dashboard');
+    Route::get('/', 'HomeController@index')->name('home');
